c0c3857aff4fcc63a997e4b73291f268c40bc6a9
Thorarinn Sigurdsson <thorarinnsigurdsson@gmail.com>
2019-12-19 12:00:42 +0000

    fix(core): allow git URLs not ending in .git

--- garden-service/src/config/common.ts
+++ garden-service/src/config/common.ts
@@ -9,7 +9,6 @@
 import Joi from "@hapi/joi"
 import { JoiObject } from "@hapi/joi"
 import { splitLast } from "../util/util"
-import isGitUrl from "is-git-url"
 import { deline, dedent } from "../util/string"
 
 export type Primitive = string | number | boolean | null
@@ -129,7 +128,7 @@ export const joi: Joi.Root = Joi.extend({
           return result.error
         }
 
-        if (!isGitUrl(value)) {
+        if (!gitUrlRegex.test(value)) {
           // tslint:disable-next-line:no-invalid-this
           return this.createError("string.gitUrl", { v: value }, state, prefs)
         }
@@ -226,6 +225,7 @@ export const absolutePathRegex = /^\/.*/ // Note: Only checks for the leading sl
 export const identifierRegex = /^(?![0-9]+$)(?!.*-$)(?!-)[a-z0-9-]{1,63}$/
 export const userIdentifierRegex = /^(?!garden)(?=.{1,63}$)[a-z][a-z0-9]*(-[a-z0-9]+)*$/
 export const envVarRegex = /^(?!garden)[a-z_][a-z0-9_\.]*$/i
+export const gitUrlRegex = /(?:git|ssh|https?|git@[-\w.]+):(\/\/)?(.*?)(\/?|\#[-\d\w._]+?)$/
 
 export const joiIdentifierDescription =
   "valid RFC1035/RFC1123 (DNS) label (may contain lowercase letters, numbers and dashes, must start with a letter, " +
